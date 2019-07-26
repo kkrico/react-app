b3ded1dda2a5632071b68f04419eb7478878a2f8
Daniel Ramos <ramos.danielferreira@gmail.com>
2019-07-25 23:36:12 -0300

    Suporte a Sass, webpack.config via promise

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./styles.scss";
 
 const App = () => {
     const f = [1, 2, 3, 4, 5];
--- webpack.config.js
+++ webpack.config.js
@@ -2,57 +2,60 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ErrorOverlayPlugin = require('error-overlay-webpack-plugin')
 
-module.exports = {
-    module: {
-        rules: [
-            {
-                enforce: "pre",
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                loader: "eslint-loader",
-                options: {
-                    emitWarning: true,
-                    configFile: "./.eslintrc.json"
-                }
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader"
-                ]
-            },
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: "html-loader"
+module.exports = new Promise(function (resolve) {
+    // TODO: Colocar esta config em algum lugar, fora do projeto rs
+    resolve({
+        module: {
+            rules: [
+                {
+                    enforce: "pre",
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    loader: "eslint-loader",
+                    options: {
+                        emitWarning: true,
+                        configFile: "./.eslintrc.json"
                     }
-                ]
-            }
-        ]
-    },
-    output: {
-        path: __dirname + '/dist',
-        filename: '[name].[chunkhash].js',
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./public/index.html",
-            filename: "./index.html"
-        }),
-        new ErrorOverlayPlugin()
-    ],
-    devServer: {
-        port: 3000
-    },
-    devtool: 'cheap-module-source-map'
-};
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        "style-loader",
+                        "css-loader",
+                        "sass-loader"
+                    ]
+                },
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader"
+                    }
+                },
+                {
+                    test: /\.html$/,
+                    use: [
+                        {
+                            loader: "html-loader"
+                        }
+                    ]
+                }
+            ]
+        },
+        output: {
+            path: __dirname + '/dist',
+            filename: '[name].[chunkhash].js',
+        },
+        plugins: [
+            new HtmlWebPackPlugin({
+                template: "./public/index.html",
+                filename: "./index.html"
+            }),
+            new ErrorOverlayPlugin()
+        ],
+        devServer: {
+            port: 3000
+        },
+        devtool: 'cheap-module-source-map'
+    })
+})
